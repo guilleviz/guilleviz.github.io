b94738e76b40f0405a79ea9b9caac5d58a54991b
Guillermo Vizcaíno Román <guillevzn@gmail.com>
2023-09-26 18:27:43 +0100

    Summary

--- html_converter.py
+++ html_converter.py
@@ -1,3 +1,3 @@
 import os
-os.chdir(r'/Users/guille/Documents/GitHub/guillevzn.github.io')
+os.chdir(r'/Users/guille/GitHub/guillevzn.github.io')
 os.system('Jupyter nbconvert cv.ipynb --to html --HTMLExporter.theme=light --no-input --no-prompt')
